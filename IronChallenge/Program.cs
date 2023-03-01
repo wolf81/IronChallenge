013188008b9e31a0dc2f4bfe8d6a60c90044bd77
Wolfgang Schreurs <wolfgang@wolftrail.net>
2023-03-01 12:54:19 +0700

    Fix project references to test project

---
+++ IronChallenge/Program.cs
@@ -0,0 +1,4 @@
+﻿using IronChallenge;
+
+// HELLO WORLD
+Console.WriteLine(Phone.OldPhonePad("4433555 555666096667775553#"));
---
+++ IronChallengeTests/PhoneTests.cs
@@ -0,0 +1,52 @@
+﻿using IronChallenge;
+
+namespace IronChallengeTests;
+
+[TestClass]
+public class PhoneTests
+{
+    [TestMethod]
+    [ExpectedException(typeof(FormatException), "Input string required")]
+    public void TestEmptyInputExceptionThrown()
+    {
+        Phone.OldPhonePad(string.Empty);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(FormatException), "Input string should be terminated by a hash (#)")]
+    public void TestInvalidInputExceptionThrown()
+    {
+        Phone.OldPhonePad("1221");
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(FormatException), "Input string contains invalid characters")]
+    public void TestInvalidCharactersExceptionThrown()
+    {
+        Phone.OldPhonePad("ab132#");
+    }
+
+    [TestMethod]
+    public void TestLetterScroll()
+    {
+        Assert.IsTrue(Phone.OldPhonePad("33#").Equals("E"));
+    }
+
+    [TestMethod]
+    public void TestLetterScrollBackspace()
+    {
+        Assert.IsTrue(Phone.OldPhonePad("227*#").Equals("B"));
+    }
+
+    [TestMethod]
+    public void TestHello()
+    {
+        Assert.IsTrue(Phone.OldPhonePad("4433555 555666#").Equals("HELLO"));
+    }
+
+    [TestMethod]
+    public void TestTuring()
+    {
+        Assert.IsTrue(Phone.OldPhonePad("8 88777444666*664#").Equals("TURING"));
+    }
+}

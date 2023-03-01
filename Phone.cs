c08542f44421e7bfcc5f4a75e6801f847185bc39
Wolfgang Schreurs <wolfgang@wolftrail.net>
2023-03-01 11:40:41 +0700

    Implement backspace

--- Phone.cs
+++ Phone.cs
@@ -10,7 +10,7 @@ namespace IronChallenge
 		/// dimension represents the index of the key. For each dimension we add
 		/// a list of associated characters.
 		/// </summary>
-        static readonly char[][] keypad = {
+        static readonly char[][] Keys = {
 			new char[] { ' ' },					// 0
 			new char[] { '&', '\'', '(' },		// 1
 			new char[] { 'A', 'B', 'C' },		// 2
@@ -21,8 +21,8 @@ namespace IronChallenge
             new char[] { 'P', 'Q', 'R', 'S' },	// 7
             new char[] { 'T', 'U', 'V' },		// 8
             new char[] { 'W', 'X', 'Y', 'Z' },	// 9
-			new char[] { '*', '\b' },			// 10 - on keypad left-hand of 9
-			new char[] { '►', '#' },			// 11 - on keypad right-hand of 9
+			//new char[] { '*', '\b' },			// 0xA - on keypad left-hand of 9
+			//new char[] { '►', '#' },			// 0xB - on keypad right-hand of 9
         };
 		
 		public static string OldPhonePad(string input)
@@ -58,13 +58,13 @@ namespace IronChallenge
 					{
 						// loop through the characters for current key, when at
 						// last character, return to first in array
-						charIndex = (charIndex + 1) % keypad[lastKeyIndex].Count();
+						charIndex = (charIndex + 1) % Keys[lastKeyIndex].Length;
 					}
 					else
 					{
 						// the key has changed, so update output string, key
 						// index & char index
-                        output = output + keypad[lastKeyIndex][charIndex];
+                        output = output + Keys[lastKeyIndex][charIndex];
                         lastKeyIndex = keyIndex;
                         charIndex = 0;
                     }
@@ -72,15 +72,32 @@ namespace IronChallenge
 				// space: reset key index, e.g. '4 44' => 'GH'
 				else if (c.Equals(' '))
 				{
-                    output = output + keypad[lastKeyIndex][charIndex];
+                    output = output + Keys[lastKeyIndex][charIndex];
                     lastKeyIndex = -1;
 				}
 				// hash: append current char to result & terminate
                 else if (c.Equals('#'))
 				{
-                    output = output + keypad[lastKeyIndex][charIndex];
+					if (lastKeyIndex != -1)
+					{
+                        output = output + Keys[lastKeyIndex][charIndex];
+                    }
                     break;
 				}
+				// backspace
+				else if (c.Equals('*'))
+				{
+					if (lastKeyIndex != -1)
+					{
+                        output = output + Keys[lastKeyIndex][charIndex];
+                    }
+
+                    if (output.Length > 1)
+					{
+						output = output.Substring(0, output.Length - 1);
+                        lastKeyIndex = -1;
+                    }
+                }
             }
 
 			return output;

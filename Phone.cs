e6400323d0d57a34ae9b7006b7f95d4cb7d6f5e9
Wolfgang Schreurs <wolfgang@wolftrail.net>
2023-03-01 09:24:45 +0700

    Implement a multi-dimensional array that represents the keys on the keypad

--- Phone.cs
+++ Phone.cs
@@ -4,6 +4,25 @@ namespace IronChallenge
 {
 	public class Phone
 	{
+        /// <summary>
+		/// This multi-dimensional array represents the keypad. 
+		/// </summary>
+        readonly char[][] keypad = {
+			new char[] { '_' },					// 0
+			new char[] { '&', '\'', '(' },		// 1
+			new char[] { 'A', 'B', 'C' },		// 2
+			new char[] { 'D', 'E', 'F' },		// 3
+            new char[] { 'G', 'H', 'I' },		// 4
+            new char[] { 'J', 'K', 'L' },		// 5
+            new char[] { 'M', 'N', 'O' },		// 6
+            new char[] { 'P', 'Q', 'R', 'S' },	// 7
+            new char[] { 'T', 'U', 'V' },		// 8
+            new char[] { 'W', 'X', 'Y', 'Z' },	// 9
+			new char[] { '*', '\b' },			// 10 - on keypad left-hand of 9
+			new char[] { '►', '#' },			// 11 - on keypad right-hand of 9
+        };
+		
+
 		public static string OldPhonePad(string input)
 		{
 			return string.Empty;
